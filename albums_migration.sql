917b45808c47419926025c42b54b33ae14d7b554
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-04-14 11:27:42 -0500

    saving progress

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,14 +1,14 @@
+warnings
 USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
-
 
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(100) DEFAULT 'NONE',
     name VARCHAR(100) NOT NULL,
-    release_date YEAR UNSIGNED NOT NULL,
+    release_date INT(4) UNSIGNED NOT NULL,
     sales FLOAT NOT NULL,
-    genre VARCHAR(100) NOT NULL,
+    genre VARCHAR(500) NOT NULL,
     PRIMARY KEY (id)
 );
