84307369321a96f7530c08196291d8255a6b12ca
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-04-13 20:27:16 -0500

    completed albums_migration.sql file/exercise

---
+++ albums_migration.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(100) DEFAULT 'NONE',
+    name VARCHAR(100) NOT NULL,
+    release_date YEAR NOT NULL,
+    sales FLOAT NOT NULL,
+    genre VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
